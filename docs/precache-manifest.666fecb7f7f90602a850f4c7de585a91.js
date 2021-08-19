153f6e2b9b83c8594209aca3253a655c5aafa36b
Jonathan <jonathan.mc.96@hotmail.com>
2021-08-19 16:23:19 -0500

    Docs agregado

---
+++ docs/precache-manifest.666fecb7f7f90602a850f4c7de585a91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b88783347ccd38fd26383c9ef24804",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f68c1138c326063d0d7",
+    "url": "/static/css/main.e67c9e18.chunk.css"
+  },
+  {
+    "revision": "e107496e2e01402b74e7",
+    "url": "/static/js/2.b11609f6.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.b11609f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "696417ce548f14b15415",
+    "url": "/static/js/3.e9ea8f66.chunk.js"
+  },
+  {
+    "revision": "8f68c1138c326063d0d7",
+    "url": "/static/js/main.ac8058e5.chunk.js"
+  },
+  {
+    "revision": "d9b5559b79fd969c1a60",
+    "url": "/static/js/runtime-main.a244f311.js"
+  }
+]);
